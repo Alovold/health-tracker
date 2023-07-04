11d3ec7d0c658a7b4f4243a18a3a4300007c74c2
Austin Lovold <austin@lovolds.com>
2023-07-04 17:52:49 -0500

    Added some recipe page logic and html

--- src/pages/recipe.page.jsx
+++ src/pages/recipe.page.jsx
@@ -2,6 +2,23 @@ import { useState, useEffect } from "react"
 
 export default function RecipePage(){
     const [recipeResponse, setRecipeResponse] = useState()
+    const [calGoal, setCalGoal] = useState();
+    const [proGoal, setProGoal] = useState();
+    const [fibGoal, setFibGoal] = useState();
+    const [sugGoal, setSugGoal] = useState();
+    const [fatGoal, setFatGoal] = useState();
+
+    const getGoals = ()=>{
+        setCalGoal(localStorage.getItem("calGoal"))
+        setProGoal(localStorage.getItem("proGoal"))
+        setFibGoal(localStorage.getItem("fibGoal"))
+        setSugGoal(localStorage.getItem("sugGoal"))
+        setFatGoal(localStorage.getItem("fatGoal"))
+    }
+
+    useEffect(()=>{
+        getGoals();
+    }, [])
 
     async function recipeFetch(input){
         const url = `https://api.edamam.com/api/recipes/v2?type=public&app_id=2330f2ef&app_key=537d2c1129aba4ab2234a239be1ebdc1&calories=300`;
@@ -21,11 +38,18 @@ export default function RecipePage(){
 
     return (
         <div>
-            <div className="attribution">
-            <div id="edamam-badge" data-color="badge"></div>
-            </div>
             <div>
+                <p>Current Calorie Goal: {localStorage.getItem("calGoal")}</p>
 
+                <p>Current Protein Goal: {localStorage.getItem("proGoal")}</p>
+
+                <p>Current Fiber Goal: {localStorage.getItem("fibGoal")}</p>
+
+                <p>Current Sugar Limit: {localStorage.getItem("sugGoal")}</p>
+
+                <p>Current Fat Limit: {localStorage.getItem("fatGoal")}</p>
+                <button>Get Recipes Based on Form</button>
+                <button>Get Recommended Recipes Based on Goals</button>
             </div>
         </div>
     )
